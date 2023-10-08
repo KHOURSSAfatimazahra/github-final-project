ee4ea9c036015518d15cbd084e26623ab6634f17
Narendra <narendrakaturi7211@gmail.com>
2023-10-08 23:29:00 +0530

    i have added read_number function for number validation

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,19 +4,40 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   #  KHOURSSAfatimazahra
+   #  KHOURSSAfatimazahra 
+   #Narendra
    # Input:
    # p, principal amount
    # t, time period in years
    # r, annual rate of interest
    # Output:
    # simple interest = p*t*r
-   echo "Enter the principal:"
-   read p
-   echo "Enter rate of interest per year:"
-   read r
-   echo "Enter time period in years:"
-   read t
-   s=`expr $p \* $t \* $r / 100`
-   echo "The simple interest is: "
-   echo $s
+   read_number() {
+  local prompt="$1"
+  local input
+  while true; do
+    echo -n "$prompt"
+    read input
+    if [[ $input =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+      echo "$input"
+      break
+    else
+      echo "Invalid input. Please enter a valid number."
+    fi
+  done
+}
+
+echo "Simple Interest Calculator"
+# Read principal amount
+p=$(read_number "Enter the principal: ")
+
+# Read rate of interest per year
+r=$(read_number "Enter rate of interest per year (%): ")
+
+# Read time period in years
+t=$(read_number "Enter time period in years: ")
+
+# Calculate simple interest
+s=$(awk "BEGIN {printf \"%.2f\", $p * $t * $r / 100}")
+
+echo "The simple interest is: $s"
